9705f7d19e1b1e042eafb014ddab7301e4154ee9
yangfq <915717502@qq.com>
2021-01-07 16:46:44 +0800

    Initial commit

---
+++ test-nacos/src/main/java/com/shiner/NacosClientApplication.java
@@ -0,0 +1,22 @@
+package com.shiner;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
+public class NacosClientApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosClientApplication.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :"+userName+"; age: "+userAge);
+    }
+
+}
